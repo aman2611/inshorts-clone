ee07979723592c2a4b603b40d6da5edf14fa27f4
aman2611 <amanprakash2611@gmail.com>
2023-07-09 15:06:19 +0530

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+      <div className="footer">
+        <span className="name">
+          Inshorts clone made by -{" "}
+          <a href="http://www.linkedin.com/in/amanprakash11" target="__blank">
+            Aman Prakash
+          </a>
+        </span>
+      </div>
+  );
+};
+
+export default Footer;
